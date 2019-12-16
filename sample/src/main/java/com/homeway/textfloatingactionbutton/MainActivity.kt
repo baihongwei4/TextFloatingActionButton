84727a7c0271cb92f1c37b28779c825ea93c0701
Mike Hongwei Bai <hongwei.bai@qantas.com.au>
2019-12-16 19:47:04 +1100

    add switch for text/icon

--- sample/src/main/java/com/homeway/textfloatingactionbutton/MainActivity.kt
+++ sample/src/main/java/com/homeway/textfloatingactionbutton/MainActivity.kt
@@ -7,22 +7,25 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var isIcon = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    override fun onResume() {
-        super.onResume()
-
-//        textFab.buttonType = ButtonType.Icon
-//        textFab.backgroundTintList = resources.getColorStateList(R.color.tint_color_primary)
-//        textFab.icon = R.drawable.ic_free_breakfast_black_24dp
-//        textFab.iconSizeRational = 1.0f
-
-        textFab.buttonType = ButtonType.Text
-        textFab.backgroundTintList = resources.getColorStateList(R.color.tint_color_primary)
-        textFab.text = "+24"
-        textFab.textAreaSizeRational = 1.0f
+        switchIconOrText.setOnClickListener {
+            isIcon = !isIcon
+            if (isIcon) {
+                textFab.buttonType = ButtonType.Icon
+                textFab.backgroundTintList = resources.getColorStateList(R.color.tint_color_primary)
+                textFab.icon = R.drawable.ic_free_breakfast_black_24dp
+                textFab.iconSizeRational = 1.0f
+            } else {
+                textFab.buttonType = ButtonType.Text
+                textFab.backgroundTintList = resources.getColorStateList(R.color.tint_color_primary)
+                textFab.text = "+24"
+                textFab.textAreaSizeRational = 1.0f
+            }
+        }
     }
 }
